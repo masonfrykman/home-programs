938802e825c65619329acdc44ea7d0700d6e015d
ikeacat <mfryk268@gmail.com>
2021-01-19 21:01:56 -0600

    Done so far.

--- backupMaintainer.py
+++ backupMaintainer.py
@@ -2,22 +2,37 @@
 
 # some variables that are going to be at the top so they can be easily changed later.
 
-CAMERA_DUMP_DIRECTORY = 'C:/Users/Mason/Documents/Cydia' # the directory that the script will be active in. start from the root folder. Use forward slashes.
-AMOUNT_TO_DELETE = 100 # the amount of data to delete IN BYTES. can go over but not under. as soon as it goes over the program kills itself.
-KILL_SELF_IF_ABOVE_FREE = 900000000000000
+CAMERA_DUMP_DIRECTORY = '' # the directory that the script will be active in. start from the root folder. Use forward slashes.
+AMOUNT_TO_DELETE = 2000000000 # the maximum amount of data to delete IN BYTES. May go over a bit just because we dont want incomplete and corrupted files. 1 gigabyte = 1000000000 bytes
+DO_DISKSPACE_CHECK = True # This is if you want to turn off the diskspace check. Turn to False if you want it off. Turn to True if you want it on.
+KILL_SELF_IF_DISKSPACE_IS_ABOVE = 10000000000 # Will check if the disk's free space is above this. If it is, the script will kill itself, if not it will proceed. 1 gigabyte = 1000000000 bytes
 
 # Essential imports to have this file work.
 
 import os
-from colorama import Fore, init, deinit
-from pathlib import Path
+from colorama import Fore, init
+from shutil import disk_usage
+
 init(autoreset=True)
 
 print('Backup Maintainer :)\n')
 
-# Ok, now check to make sure the directory exists.
+# First, check if the Diskspace check is on, then do stuff accordingly.
 
-print(Fore.CYAN + 'Checking that the specified directory exists...')
+if(DO_DISKSPACE_CHECK == True):
+    print(Fore.CYAN + 'Running Diskspace Check...')
+    totalFree = disk_usage(CAMERA_DUMP_DIRECTORY).free
+    if(totalFree > KILL_SELF_IF_DISKSPACE_IS_ABOVE):
+        print(Fore.YELLOW + 'Disk space free is above the defined byte amount. Killing self.')
+        raise SystemExit
+    else:
+        print(Fore.GREEN + 'Disk Space')
+else:
+    print(Fore.CYAN + 'Diskspace Check is disabled.')
+
+# Ok, now check to make sure the directory exists.s
+
+print(Fore.CYAN + '\nChecking that the specified directory exists...')
 
 checkPathForExistence = os.path.isdir(CAMERA_DUMP_DIRECTORY)
 
@@ -49,7 +64,7 @@ if(CAMERA_DUMP_DIRECTORY.endswith('/') == False): # This is to make sure that wh
 else:
     CAMERA_DUMP_DIRECTORY_MOD = CAMERA_DUMP_DIRECTORY
 
-d = []
+d = [] # The full paths will be dumped here.
 i = 0
 
 for filename in filesInCD:
@@ -66,3 +81,20 @@ print(Fore.GREEN + 'Sorted!')
 
 # Now start the deleting!!!
 
+print(Fore.CYAN + '\nStarting to delete ' + str(AMOUNT_TO_DELETE) + " bytes of the oldest files in '" + CAMERA_DUMP_DIRECTORY + "'.")
+
+if(AMOUNT_TO_DELETE <= 0):
+    print('ERROR! AMOUNT_TO_DELETE cannot be 0 or lower. Change the value to be above zero please.')
+
+amountOfBytesDeleted = 0
+
+for fileTD in d:
+    b = os.stat(fileTD).st_size
+    os.remove(fileTD)
+    amountOfBytesDeleted += b
+    if(amountOfBytesDeleted > AMOUNT_TO_DELETE):
+        break
+
+print(Fore.GREEN + 'Successfully deleted ' + str(amountOfBytesDeleted) + ' bytes of data!')
+print(Fore.YELLOW + 'Please note: The script may have gone over the maximum because partially deleting a file will leave it corrupted. If it went under, we probably ran out of files to delete.')
+print('Have a great rest of your day/night <3')
